17f31998664efc033e9e7c9a3f2382116e0d3a64
Pablo Agreda <pablo.agreda@galileo.edu>
2023-04-07 10:15:07 -0700

    Primer push desde linux

--- threads/thread.h
+++ threads/thread.h
@@ -24,6 +24,8 @@ typedef int tid_t;
 #define PRI_DEFAULT 31                  /* Default priority. */
 #define PRI_MAX 63                      /* Highest priority. */
 
+//*****************************Implementacion NICE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 /* A kernel thread or user process.
 
    Each thread structure is stored in its own 4 kB page.  The
@@ -89,7 +91,9 @@ struct thread
     uint8_t *stack;                     /* Saved stack pointer. */
     int priority;                       /* Priority. */
     struct list_elem allelem;           /* List element for all threads list. */
-
+    
+    //*****************************Implementacion NICE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+   
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
 
