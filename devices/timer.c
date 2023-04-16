d199cc2ef2a112a3a2c54b59bff5dc242a0b9f19
Pablo Agreda <pablo.agreda@galileo.edu>
2023-04-15 23:07:28 -0700

    Implementacion de Alarm Clock

--- devices/timer.c
+++ devices/timer.c
@@ -92,8 +92,11 @@ timer_sleep (int64_t ticks)
   int64_t start = timer_ticks ();
 
   ASSERT (intr_get_level () == INTR_ON);
-  while (timer_elapsed (start) < ticks) 
-    thread_yield ();
+  /*while (timer_elapsed (start) < ticks) 
+    thread_yield ();*/
+  //--------------------------------------------
+  func_dormir_threads(ticks); //insertar_en_lista_espera
+  //--------------------------------------------
 }
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be
@@ -172,6 +175,9 @@ timer_interrupt (struct intr_frame *args UNUSED)
 {
   ticks++;
   thread_tick ();
+//-------------------------------------------------------
+  func_despertar_threads(ticks); //remover_thread_durmiente
+//-------------------------------------------------------
 }
 
 /* Returns true if LOOPS iterations waits for more than one timer
